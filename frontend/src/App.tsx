d44a542285b2ddf0eaa5ae06a8dd626bdeba901f
owati <mofiyinfoluwaowati@gmail.com>
2024-08-21 13:22:25 +0100

    initial and final commmit 😎

---
+++ frontend/src/App.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { io } from 'socket.io-client';
+
+interface FlowRateData {
+  input: number,
+  output: number,
+  capacity: number
+}
+function App() {
+  const [{
+    capacity,
+    input,
+    output
+  }, setFlowRate] = React.useState<FlowRateData>({
+    input: 0,
+    output: 0,
+    capacity: 0
+  });
+
+  React.useEffect(
+    () => {
+      const socket = io('/')
+
+      socket.on("connect", () => {
+        console.log("Connected to server")
+      })
+
+      socket.on("data", (data: any) => {
+        setFlowRate(data)
+      })
+
+      return function cleanup() {
+        socket.disconnect()
+      }
+    }, []
+  )
+  return <div className='bg-slate-700 w-full h-screen'>
+
+    {/*  Navbar */}
+    <nav className='shadow-xl'>
+      <div className='container mx-auto px-2 py-4'>
+        <h1 className='text-white text-2xl'>Flow rate sensor panel</h1>
+      </div>
+    </nav>
+
+
+    {/* Tank display */}
+    <div className='h-[calc(100vh-64px)] container mx-auto px-2 py-4 flex md:scale-100 scale-75'>
+
+      <div className='border-x-2 border-b w-full max-w-72 h-96 m-auto relative flex items-end'>
+
+        <div className={`border-y-2 w-20 absolute -left-20 h-8 ${output ? "bg-white/50" : "bg-slate-700"} top-10`}>
+          <p className='absolute text-sm -top-7 w-full text-center text-white'>{output.toFixed(2)}L/min</p>
+        </div>
+
+        <div className={`border-y-2 w-20 absolute -right-20 h-8 ${input ? "bg-white/50" : "bg-slate-700"} bottom-10`}>
+          <p className='absolute text-sm -top-7 w-full text-center text-white'>{input.toFixed(2)}L/min</p>
+        </div>
+
+        {/* Quantity */}
+        <div className='bg-white/50 w-full relative transition-all duration-700' style={{
+          height : `${(capacity / 25) * 100}%`
+        }}>
+          <p className='absolute -top-9 text-2xl w-full text-center text-white'>{capacity.toFixed(2)} Litres</p>
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default App;
---
+++ src/app.py
@@ -0,0 +1,29 @@
+import json
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+socket = SocketIO(app)
+cors = CORS(app)
+
+FLOW_RATE_DATA_KEY="flow.rate.data.key"
+
+
+@socket.on("connect")
+def on_connect():
+    print("New connection")
+
+@app.route("/")
+def index_page():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.post('/update-flow-rate')
+def update_flow_rate():
+    try:
+        data = json.loads(request.data)
+        socket.emit("data", data)
+        return jsonify({"message" : "OK"}), 200
+    except:
+        return jsonify({"message" : "Failed"}), 400
